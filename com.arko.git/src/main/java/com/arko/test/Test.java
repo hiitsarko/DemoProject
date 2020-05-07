ec2044bf859a577fc2e6d3620477c2d2c4536091
Arko <hi.itsarko@gmail.com>
2020-05-07 15:02:13 +0530

    normal commit

--- com.arko.git/src/main/java/com/arko/test/Test.java
+++ com.arko.git/src/main/java/com/arko/test/Test.java
@@ -1,11 +1,51 @@
 package com.arko.test;
 
+
+
+interface TouchScreenLaptop{
+	void scroll();
+	void click();
+}
+abstract class HP implements TouchScreenLaptop{
+
+	public void scroll() {
+		System.out.println("Hp implementions");
+		
+	}
+	
+}
+abstract class DELL implements TouchScreenLaptop{
+
+	public void scroll() {
+		System.out.println("DELL implementions");
+		
+	}
+	
+}
+class HPNotebook extends HP{
+
+	public void click() {
+		System.out.println("HP Click implemetion");
+		
+	}
+	
+}
+class DELLNotebook extends HP{
+
+	public void click() {
+		System.out.println("DELL Click implemetion");
+		
+	}
+	
+}
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("Arko ghosh");
-
+		TouchScreenLaptop obj1 = new HPNotebook();
+		TouchScreenLaptop obj2 = new DELLNotebook();
+		obj1.click();
+		obj2.click();
 	}
-
 }
+
+
